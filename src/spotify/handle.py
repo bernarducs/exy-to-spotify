e484113069f019c501b8a5d4b2cf3e3d4241af93
bernarducs <bernarducs@gmail.com>
2024-07-21 02:10:02 -0300

    add thefuzz lib to check album titles matches

--- src/spotify/handle.py
+++ src/spotify/handle.py
@@ -1,6 +1,4 @@
-from datetime import datetime, timezone
-from dataclasses import dataclass, field
-
+from thefuzz import fuzz
 from .auth import cursor
 
 
@@ -14,20 +12,22 @@ def add_album(albums):
     for album in albums:
 
         artist, title = album.title.split(' – ')
+        title, year = title.rsplit(' ', 1)
+        year = year[1:-1]
 
         query = sp.search(q=f'album: {title} artist: {artist}', type='album')
         items = query['albums']['items']
 
-        if items:
-            album_uri = items[0]['uri']
-            sp.current_user_saved_albums_add([album_uri])
-            success_add_albums.append(album)
-            print(f'\n\n{title}, from {artist} was successfully added')
+        if item:= items[0]:
+            album_uri = item['uri']
+            album_already_added = sp.current_user_saved_albums_contains([album_uri])[0]
 
-        else:
-            print(f"\n\n{title}, from {artist} wasn't successfully added")
-
-    print(
-        f'\n\n\n{len(success_add_albums)} was added to your spotify library.'
-    )
+            artist_ratio = fuzz.ratio(artist, item['artists'][0]['name'])
+            album_ratio = fuzz.ratio(title, item['name'])
+            year_item = item['release_date'][:4]
+            
+            if not album_already_added and artist_ratio > 98 and album_ratio > 60 and year == year_item:
+                sp.current_user_saved_albums_add([album_uri])
+                success_add_albums.append(album)
+     
     return success_add_albums
